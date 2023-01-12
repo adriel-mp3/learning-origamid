9dade56044e8f6ad0510b55e3939ea37f2f76e62
Adriel Santos <109179771+adriel-mp3@users.noreply.github.com>
2023-01-11 21:46:19 -0300

    add new exercise

---
+++ 4-efeitos-no-dom/modal/js/script.js
@@ -0,0 +1,13 @@
+// import <nome> from <caminho>
+import initTabNav from "./modules/tabmenu.js";
+import initAccordion from "./modules/accordion.js";
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll  from "./modules/scroll-animacao.js";
+import initModal from "./modules/modal.js";
+
+initTabNav()
+initAccordion()
+initScrollSuave()
+initAnimacaoScroll()
+initModal()
+
